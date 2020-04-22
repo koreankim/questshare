64fa75d72f86230d1421fa92755c0dd24dc50a49
Junho Kim <jnhkm311@gmail.com>
2020-04-21 18:30:40 -0700

    update error page

--- client/src/components/error/Error.js
+++ client/src/components/error/Error.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Result, Button } from "antd";
 
 class Error extends React.Component {
-    render () {
-        return <div>This page doesn't exist!</div> 
-    };
+  handleClick(path) {
+    this.props.history.push(path);
+  }
+
+  render() {
+    return (
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={<Button type="primary" onClick={(e) => this.handleClick("/")}>Back Home</Button>}
+      />
+    );
+  }
 }
 
 export default Error;
