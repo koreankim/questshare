027922502043e96a0eee967a4c2ada0008d1ba2a
Junho Kim <jnhkm311@gmail.com>
2020-04-27 16:10:43 -0700

    Remove private config file and adsense script

--- server/api/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    ENV = "production"
-    MONGO_URI='mongodb+srv://admin:questshare1111@questshare-prod-ry5hm.mongodb.net/test?retryWrites=true&w=majority'
